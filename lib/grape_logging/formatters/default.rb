eeed61da912e5d5e41081cfe18476b731a96f4d3
Guseynov Pavel <pavel.guseynov@gmail.com>
2015-04-22 15:56:24 +0300

    rails style formatter

--- lib/grape_logging/formatters/default.rb
+++ lib/grape_logging/formatters/default.rb
@@ -11,7 +11,7 @@ module GrapeLogging
         elsif data.is_a?(Exception)
           format_exception(data)
         elsif data.is_a?(Hash)
-          "#{data.delete(:status)} -- total=#{data.delete(:total)} db=#{data.delete(:db)} -- #{data.delete(:method)} #{data.delete(:path)} #{format_hash(data)}"
+          "Completed #{data.delete(:status)} in #{data.delete(:total)} (ActiveRecord:#{data.delete(:db)}) #{data.delete(:method)} #{data.delete(:path)} #{format_hash(data)}"
         else
           data.inspect
         end
